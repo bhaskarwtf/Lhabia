500f9615a013880452b884f52372cd29b889346d
bhaskartf <mr.unkown3166@gmail.com>
2026-05-17 18:06:38 +0530

    Added system arugments function and error handaling function

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-import socket 
+import socket
+import time
+import sys
 
-ip = str(input("Tell me the target ip??")) 
-port = 0
+ip=sys.argv[1]
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <ip>")
+    sys.exit()
+    
+start_port = 0
 end_port = 1023
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-for i in range(port, end_port):
-   
-   port += 1
-   rel = server.connect_ex((ip, port))
-   if rel == 0:
-      print(f"Port is open {port}")
-   
+
+def tcp():
+    print("Starting TCP scan .....")
+    time.sleep(5)
+    for port in range(start_port, end_port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.settimeout(0.5)
+            rel = server.connect_ex((ip, port))
+            if rel == 0:
+                print(f"Port is open {port}")
+
+
+if __name__ == "__main__":
+    tcp()
