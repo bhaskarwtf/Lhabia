2f94c49b18034c2758f5ca407c45abd60950dbeb
bhaskartf <mr.unkown3166@gmail.com>
2026-05-29 14:47:42 +0530

    banner grabber

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+from concurrent.futures import ThreadPoolExecutor
 import socket
-import time
 import sys
 
-ip=sys.argv[1]
+if len(sys.argv) != 2:
+    print(f"Usage: python {sys.argv[0]} <target_ip>")
+    sys.exit(1)
 
-if len(sys.argv[1]) < 2:
-    print("Usage: python3 main.py <ip>")
-    sys.exit()
-    
-start_port = 0
-end_port = 1023
+ip = sys.argv[1]
 
 
-def tcp():
-    print("Starting TCP scan .....")
-    time.sleep(5)
-    for port in range(start_port, end_port):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-            server.settimeout(0.5)
-            rel = server.connect_ex((ip, port))
-            if rel == 0:
+def tcp(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.settimeout(0.5)
+        rel = server.connect_ex((ip, port))
+        if rel == 0:
+            try:
+                banner = server.recv(1024).decode().strip()
+                print(f"Port is open {port} : {banner}")
+            except socket.timeout:
                 print(f"Port is open {port}")
+        
 
 
-if __name__ == "__main__":
-    tcp()
+
+with ThreadPoolExecutor(max_workers=100) as executor:
+    executor.map(tcp, range(0, 1024))
+    
