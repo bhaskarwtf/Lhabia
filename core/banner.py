bdc96ebdbc5ed39f2928218e3965d1213ae0189c
bhaskartf <mr.unkown3166@gmail.com>
2026-05-30 22:02:17 +0530

    added command file and https_grabber

--- core/banner.py
+++ core/banner.py
@@ -1,4 +1,5 @@
 import socket
+import ssl
 
 def grab_banner(connected_socket, ip, port):
     
@@ -7,34 +8,55 @@ def grab_banner(connected_socket, ip, port):
         if banner:
             return banner
     except Exception:
-        pass  
+        pass
 
-    if port in [80, 8080, 443]:  
+    if port in [80, 8080]:
         return grab_http_banner(ip, port)
+    if port in [443, 8443]:
+        return https_grabber(ip, port)
 
     return "TCP open (No banner received)"
 
 
 def grab_http_banner(ip, port):
-    
     try:
-        
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as http_sock:
             http_sock.settimeout(1.5)
             http_sock.connect((ip, port))
-            
 
             http_request = f"HEAD / HTTP/1.1\r\nHost: {ip}\r\nUser-Agent: NetScanner/1.0\r\nConnection: close\r\n\r\n"
             http_sock.sendall(http_request.encode())
-            
-            
+
             response = http_sock.recv(2048).decode(errors='ignore')
-            
 
             for line in response.split("\r\n"):
                 if line.lower().startswith("server:"):
                     return f"HTTP Server: {line.split(':', 1)[1].strip()}"
-                    
             return "HTTP open (Server header missing)"
-    except Exception as e:
-        return f"HTTP open (Request failed)"
+    except Exception:
+        return "HTTP open (Request failed)"
+
+
+def https_grabber(ip, port):
+    try:
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(5)
+            sock.connect((ip, port))
+
+            with context.wrap_socket(sock, server_hostname=ip) as tls_sock:
+                tls_sock.settimeout(5)
+                ssl_request = f"HEAD / HTTP/1.1\r\nHost: {ip}\r\nUser-Agent: NetScanner/1.0\r\nConnection: close\r\n\r\n"
+                tls_sock.sendall(ssl_request.encode())
+
+                ssl_response = tls_sock.recv(2048).decode(errors='ignore')
+
+                for line in ssl_response.split("\r\n"):
+                    if line.lower().startswith("server:"):
+                        return f"HTTPS Server: {line.split(':', 1)[1].strip()}"
+                return "HTTPS open (Server header missing)"
+    except Exception:
+        return "HTTPS open (Request failed)"
