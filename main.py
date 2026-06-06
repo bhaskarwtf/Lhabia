623780e7f271c48dbb05a933e54ad01338902b95
bhaskartf <mr.unkown3166@gmail.com>
2026-06-06 11:25:46 +0530

    organised main.py for argparse

--- main.py
+++ main.py
@@ -1,30 +1,58 @@
 from concurrent.futures import ThreadPoolExecutor
 import socket
-import sys
-from core.banner import grab_banner  
 
-if len(sys.argv) < 2:
-    print(f"Usage: python {sys.argv[0]} <target_ip> [-p-]")
-    sys.exit(1)
+from core.argprase import parse_arguments
+from core.banner import grab_banner
+from scanners.syn import SynScanner
 
-ip = sys.argv[1]
+args = parse_arguments()
+ip = args.ip
+scan_type = "-sT" if args.tcp_connect else "-sS" if args.syn_scan else None
+scan_all_ports = args.p == "1-65535"
 
-scan_all_ports = "-p-" in sys.argv[2:]
+
 start_port = 1
 end_port = 65535 if scan_all_ports else 1024
 
-def tcp(port):
+
+def tcp_scan(port):
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.settimeout(1.0) 
+
+        server.settimeout(1.0)
+
         result = server.connect_ex((ip, port))
-        
+
         if result == 0:
-            
-            banner = grab_banner(server, ip, port)
-            print(f"[+] Port {port} is open: {banner}")
+            if scan_type == "-sV":
+                banner = grab_banner(server, ip, port)
+                print(f"[+] Port {port} open with {banner}")
+            else:
+                print(f"[TCP] Port {port} open")
+
+
+def syn_scan(port):
+
+    result = SynScanner.scan(ip, port)
+
+    if result == "Open":
+        print(f"[SYN] Port {port} open")
+
 
 if __name__ == "__main__":
-    print(f"Scanning {ip} from port {start_port} to {end_port}...")
-    
+
+    print(f"Scanning {ip} from {start_port} to {end_port}")
+
     with ThreadPoolExecutor(max_workers=100) as executor:
-        executor.map(tcp, range(start_port, end_port + 1))
+
+        if scan_type == "-sT":
+            executor.map(tcp_scan, range(start_port, end_port + 1))
+
+        elif scan_type == "-sS":
+            executor.map(syn_scan, range(start_port, end_port + 1))
+        
+        elif scan_type == "-sV":
+            executor.map(tcp_scan,range(start_port,end_port +1))
+
+        else:
+            print("Invalid scan type")
