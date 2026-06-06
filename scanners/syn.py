e56c5a631aee965299c8ee9047ae3554465905fa
bhaskartf <mr.unkown3166@gmail.com>
2026-06-06 21:35:50 +0530

    did some fix  , not working

--- scanners/syn.py
+++ scanners/syn.py
@@ -1,4 +1,4 @@
-from scapy.all import *
+from scapy.all import IP, TCP, sr, send
 import random
 
 class SynScanner:
@@ -11,20 +11,18 @@ class SynScanner:
         packet = IP(dst=ip) / TCP(
             sport=src_port,
             dport=port,
-            flags='S'
+            flags='S',
+            seq=random.randint(0, 4294967295)
         )
 
-        response = sr1(packet, timeout=1, verbose=0)
+        
+        response = sr(packet, timeout=2, verbose=0)
+        if not response:
+            return "Filtered"  
 
-        if response is None:
-            return "Filtered"
-
-        if response.haslayer(TCP):
-
-            flags = response[TCP].flags
 
             # SYN-ACK
-            if flags == 0x12:
+        if flags == 0x12:
 
                 rst_packet = IP(dst=ip) / TCP(
                     sport=src_port,
@@ -37,10 +35,7 @@ class SynScanner:
                 return "Open"
 
             # RST-ACK
-            elif flags == 0x14:
+        elif flags == 0x14:
                 return "Closed"
 
         return "Unknown"
-    
-    
-    
