f9ff8545f4908b7f750b6bfd767af19003bcad29
bhaskartf <mr.unkown3166@gmail.com>
2026-06-07 15:42:01 +0530

    changed syn scanner

--- main.py
+++ main.py
@@ -49,8 +49,16 @@ if __name__ == "__main__":
 
         if scan_type == "-sT":
             executor.map(tcp_scan, range(start_port, end_port + 1))
+        
         elif scan_type == "-sS":
-            executor.map(syn_scan, range(start_port, end_port + 1))
+            scanner = SynScanner(
+            target_ip=ip,
+            ports=range(start_port, end_port + 1),
+            timeout=3,
+            rate=500
+            )
+            scanner.scan()
+    
         elif scan_type == "-sV":
             executor.map(tcp_scan,range(start_port,end_port +1))
 
